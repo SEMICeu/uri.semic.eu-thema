37427c323027b9922b122d0e4c3fbd067eb879ca
EmidioStani <emidiostani@gmail.com>
2024-01-08 14:19:41 +0100

    fixed example.js

--- site-skeleton/core-business/example.js
+++ site-skeleton/core-business/example.js
@@ -153,7 +153,7 @@ $(document).ready(function () {
 		var index = myIndexOf(indexValues, exampleid);
 
 		newUrl = "https://json-ld.org/playground/#startTab=tab-expand&json-ld=" + encodeURIComponent(editors[index].CM1.getValue()); 
-		window.open(encodeURI(newUrl), '_blank');
+		window.open(newUrl, '_blank');
 		return false;
 	});
 	
