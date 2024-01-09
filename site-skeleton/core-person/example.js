192b98d23bd44a57a80e8bad6e565cc70af0f226
EmidioStani <emidiostani@gmail.com>
2024-01-09 15:14:52 +0100

    invert values

--- site-skeleton/core-person/example.js
+++ site-skeleton/core-person/example.js
@@ -153,7 +153,7 @@ $(document).ready(function () {
 		var indexValues = $examples.map(function() { return this.id; }) ;
 		var index = myIndexOf(indexValues, exampleid);
 
-		newUrl = "https://json-ld.org/playground/#startTab=tab-expand&json-ld=" + encodeURIComponent(editors[index].CM0.getValue()); 
+		newUrl = "https://json-ld.org/playground/#startTab=tab-expand&json-ld=" + encodeURIComponent(editors[index].CM1.getValue()); 
 		window.open(newUrl, '_blank');
 		return false;
 	});
@@ -162,7 +162,7 @@ $(document).ready(function () {
 		var indexValues = $examples.map(function() { return this.id; }) ;
 		var index = myIndexOf(indexValues, exampleid);
 
-		newUrl = "https://converter.zazuko.com/#value=" + encodeURIComponent(editors[index].CM1.getValue()) + "&format=text%2Fturtle"; 
+		newUrl = "https://converter.zazuko.com/#value=" + encodeURIComponent(editors[index].CM0.getValue()) + "&format=text%2Fturtle"; 
 		window.open(newUrl, '_blank');
 		return false;
 	});
